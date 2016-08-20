46e9f473dc488378ea539e7633f3cd0f6d9ecae7
andy <andy2472625@gmail.com>
2016-08-20 15:05:26 +0800

    andy

--- src/js/common.js
+++ src/js/common.js
@@ -20,7 +20,7 @@
 	//Event
 	function window_deviceorientation(e) {
 		var dis = Math.floor(o.street_width / 360 * Math.floor(e.alpha - 180)) * -1;
-		$('.tip_box').html(Math.floor(e.alpha - 180) + 'dis2:' + dis);
+		$('.tip_box').html(Math.floor(e.alpha - 180) + 'dis3:' + dis);
 		$('.street_all').css('left',dis);
 	}
 	
