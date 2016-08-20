63b3beb3b525068993b009a79432783ace457d77
andy <andy2472625@gmail.com>
2016-08-20 15:48:32 +0800

    a

--- src/js/common.js
+++ src/js/common.js
@@ -2,17 +2,18 @@
 	//Init
 	var o ={
 		wrp: $('.wrapper'),
-		loading: $('.loading'),
-		street_width: $('.street').width()
+		loading: $('.loading')
 	};
-	$('.street').eq(1).css('left',o.street_width);
-	$('.street_all').css('margin-left',o.street_width*-1);
+	
 
 
 	//AddListener
 	$(window).load(window_load);
 	function window_load(){
 		o.loading.fadeOut();
+		o.street_width = $('.street').eq(0).width();
+		$('.street').eq(1).css('left',o.street_width);
+		$('.street_all').css('margin-left',o.street_width*-1);
 		window.addEventListener('deviceorientation', function(e){
 			window_deviceorientation(e);
 		});
@@ -22,7 +23,6 @@
 
 	//Event
 	function window_deviceorientation(e) {
-		o.street_width = $('.street').eq(0).width();
 		var dis = Math.floor(o.street_width / 360 * Math.floor(e.alpha - 180)) * -1;
 		$('.tip_box').html(Math.floor(e.alpha - 180) + 'dis7:' + dis);
 		$('.street_all').css('left',dis);
