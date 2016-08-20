9ef8e340c1a86e330ed944d8305d62bfbd111bca
andy <andy2472625@gmail.com>
2016-08-20 12:49:34 +0800

    andy

--- src/js/common.js
+++ src/js/common.js
@@ -1,8 +1,22 @@
 ﻿$(document).ready(function(){
 	//Init
 	var o ={
-		wrp: $('.wrapper')
+		wrp: $('.wrapper'),
+		loading: $('.loading')
 	};
+
+	//AddListener
+	$(window).load(window_load);
+	function window_load(){
+		o.loading.fadeOut();
+	}
+	window.addEventListener('deviceorientation', window_deviceorientation);
+
+
+	//Event
+	function window_deviceorientation(e) {
+		$('.tip_box').html(e.gamma);
+	}
 	
 	
 })//ready end  
